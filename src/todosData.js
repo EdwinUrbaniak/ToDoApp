037a8a35248f28aae7db22cbd3e4fb5c9e2d2cc8
Edwin <urbaniak_edwin@wp.pl>
2019-09-18 22:45:42 +0200

    initial commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Wynieść śmieci",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Iść na zakupy",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Posprzątać dom",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Skosić trawnik",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Poczytać książkę",
+        completed: false
+    }
+]
+
+export default todosData
